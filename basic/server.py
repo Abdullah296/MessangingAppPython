79229b54e1411b6412145af4b36eba3c5e7febc0
abd296 <46623916+abd296@users.noreply.github.com>
2020-06-17 15:15:09 +0500

    Basic code

---
+++ basic/server.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  9 11:50:10 2020
+
+@author: Abdullah
+"""
+
+import socket as S
+import threading as T
+import tqdm
+import os
+
+Clients = []
+Client = {}
+
+def handler(client_socket,client_address):
+    print("Connected by",client_address)
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            break
+        else:
+             if data.decode("utf-8") == 'file':
+                 recievefile(client_socket)
+             else:
+                 data =  " " + str(Client[client_address[1]]) + " --> " + data.decode("utf-8")
+                 SendMsg(client_socket , data)
+def recievefile(ClientSocket):
+    #print('hello1')
+    BUFFER_SIZE = 4096
+    received = ClientSocket.recv(BUFFER_SIZE).decode()
+    filename, filesize = received.split()
+    filename = os.path.basename(filename)
+    filesize = int(filesize)
+    progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    print('hello')
+    with open(filename, "wb") as f:
+        for _ in progress:
+            bytes_read = ClientSocket.recv(BUFFER_SIZE)
+            if not bytes_read:    
+                break
+            f.write(bytes_read)
+            progress.update(len(bytes_read))
+         
+def SendMsg(clientS,msg):
+    for client in Clients:
+        if client != clientS:
+            client.send(msg.encode("utf-8"))
+            
+host = 'localhost'
+port = 12345
+Server = S.socket(S.AF_INET,S.SOCK_STREAM)
+Server.bind((host,port))
+Server.listen(3)
+
+print("Server is Online:")
+while True:
+    client_S,client_A = Server.accept()
+    data = client_S.recv(1024)
+    data = data.decode("utf-8")
+    Client.update({client_A[1] : data})    
+    Clients.append(client_S)
+    
+    print(Client[client_A[1]])
+    
+    new_thread = T.Thread(target=handler,args=(client_S,client_A))
+    new_thread.start()
+    
+    
+    
+    
+    
